ed46c279b0d0a048e0c63833ae92c5c8a482293b
Megan McLean <425megs@gmail.com>
2022-01-31 19:31:00 -0800

    get started button working, can write new notes and save to array, displays when server is refreshed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Note-Taker
+
+## User Story
+
+```
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+```
+
+## Description
+
+Your
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+// const savedNotes = require('./db/db.json');
+const fs = require('fs');
+
+//grant unique id, don't think it will live on this page
+// const { v4: uuidv4 } = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+let notes = JSON.parse(fs.readFileSync("db/db.json"))
+
+// nav to notes page after clicking 'Get Started'
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', function (req, res) {
+  return res.json(notes)
+});
+
+app.post('/api/notes', function (req, res) {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      throw err
+    }
+    const noteArray = JSON.parse(data);
+    noteArray.push(req.body)
+    fs.writeFile("./db/db.json", JSON.stringify(noteArray, null, 4), err => {
+      if (err) {
+        throw err;
+      }
+    })
+  })
+}
+);
+
+app.listen(PORT, () =>
+  console.log(`You are now listening to the sweet tunes of PORT 3001 at http://localhost:${PORT} 🚀`)
+);
